f7d242e4fb12f018d480f8143f2977db7c8f1398
Michal Melnikovic <melnikovic.m@gmail.com>
2016-09-23 13:19:46 +0200

    change date only if no date is inserted

--- controllers/book_controller.js
+++ controllers/book_controller.js
@@ -8,7 +8,7 @@ var bookController = function(Book) {
       res.send('Title is required');
     } else {
       // Added current datetime when creating
-      book.createdDate = new Date();
+      if(!req.body.createdDate) book.createdDate = new Date();
       book.save();
 
       // Separated those calls so the tests can sucess
